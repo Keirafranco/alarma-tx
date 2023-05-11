20372c0a0acc59b31cfb273c57797ea63e6c4c06
Keirafranco <125355714+Keirafranco@users.noreply.github.com>
2023-05-11 09:37:24 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-radio.setGroup(1)
+radio.setGroup(115)
 music.setVolume(35)
 basic.forever(function () {
-    if (input.magneticForce(Dimension.X) < 600) {
-        radio.sendNumber(0)
+    if (input.magneticForce(Dimension.Strength) < 600) {
+        radio.sendNumber(1)
         basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            . . . . #
+            . . . # .
+            # . # . .
+            # # . . .
+            . . . . .
             `)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
         basic.pause(200)
         basic.clearScreen()
         basic.pause(200)
